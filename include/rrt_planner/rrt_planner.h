dc1f52408b68b6180bdace322e945029c52b5a7b
Erlend Veire <erlend.veire@gmail.com>
2024-10-08 01:54:57 -0700

    RRT-planner

--- include/rrt_planner/rrt_planner.h
+++ include/rrt_planner/rrt_planner.h
@@ -8,90 +8,88 @@
 #include <rrt_planner/utils.h>
 #include <vector>
 
-namespace rrt_planner {
+namespace rrt_planner
+{
 
-    class RRTPlanner {
+    class RRTPlanner
+    {
 
-        public:
-            /**
-             * @brief Constructor for the RRTPlanner object.
-             * @param costmap A pointer to the costmap to use for planning.
-             * @param params Struct with parameters to build the tree.
-             */
-            RRTPlanner(costmap_2d::Costmap2DROS *costmap, const rrt_params& params);
+    public:
+        /**
+         * @brief Constructor for the RRTPlanner object.
+         * @param costmap A pointer to the costmap to use for planning.
+         * @param params Struct with parameters to build the tree.
+         */
+        RRTPlanner(costmap_2d::Costmap2DROS *costmap, const rrt_params &params);
 
-            /**
-             * @brief   Plan a path using RRT
-             * @return  True if found, false otherwise 
-             */
-            bool planPath();
-            
-            /**
-             * @brief Sample random points
-             * @return 2D random planar position (x, y)
-             */
-            double *sampleRandomPoint();
+        /**
+         * @brief   Plan a path using RRT
+         * @return  True if found, false otherwise
+         */
+        bool planPath();
 
-            /**
-             * @brief Get the index of the nearest node around the new random point
-             * @param point Random pointed sampled
-             * @return The nearest node index
-             */
-            int getNearestNodeId(const double *point);
+        /**
+         * @brief Sample random points
+         * @return 2D random planar position (x, y)
+         */
+        double *sampleRandomPoint();
 
-            /**
-             * @brief Create new node
-             * @param pos 2D coordinates of the new node
-             * @param parent_node_id Id of the parent node of the new node
-             */
-            void createNewNode(const double* pos, int parent_node_id);
+        /**
+         * @brief Get the index of the nearest node around the new random point
+         * @param point Random pointed sampled
+         * @return The nearest node index
+         */
+        int getNearestNodeId(const double *point);
 
-            /** 
-             * @brief Connect the sampled random point to nearest tree node.
-             * @param start 2D coordinates of nearest existing tree node to random point
-             * @param goal  2D coordinates of sampled random point
-             * @return 2D coordinates of potential new tree node
-             */
-            double *extendTree(const double* point_nearest, const double* point_rand);
+        /**
+         * @brief Create new node
+         * @param pos 2D coordinates of the new node
+         * @param parent_node_id Id of the parent node of the new node
+         */
+        void createNewNode(const double *pos, int parent_node_id);
 
-            /*********************
-             * Start and goal positions
-             *********************/
+        /**
+         * @brief Connect the sampled random point to nearest tree node.
+         * @param start 2D coordinates of nearest existing tree node to random point
+         * @param goal  2D coordinates of sampled random point
+         * @return 2D coordinates of potential new tree node
+         */
+        double *extendTree(const double *point_nearest, const double *point_rand);
 
-            /**
-             * @brief   Sets the start position for the planner
-             * @param   start The start position
-             */
-            void setStart(double *start);
+        /*********************
+         * Start and goal positions
+         *********************/
 
-            /**
-             * @brief   Sets the goal position for the planner
-             * @param   start The goal position
-             */
-            void setGoal(double *goal);
+        /**
+         * @brief   Sets the start position for the planner
+         * @param   start The start position
+         */
+        void setStart(double *start);
 
-            /**
-             * @brief Get the RRT tree.
-             */
-            const std::vector<Node>& getTree();
+        /**
+         * @brief   Sets the goal position for the planner
+         * @param   start The goal position
+         */
+        void setGoal(double *goal);
 
-            ~RRTPlanner() {};
+        /**
+         * @brief Get the RRT tree.
+         */
+        const std::vector<Node> &getTree();
 
-            
-            private:
-                double start_[2], goal_[2];
-                double rand_point_[2], candidate_point_[2];
-                float map_width_, map_height_;
-                costmap_2d::Costmap2D* costmap_;
-                std::vector<Node> nodes_;
-                rrt_params params_;
-                CollisionDetector collision_dect_;
-                RandomDoubleGenerator random_double_x, random_double_y;
+        ~RRTPlanner() {};
 
+    private:
+        double start_[2], goal_[2];
+        double rand_point_[2], candidate_point_[2];
+        float map_width_, map_height_;
+        costmap_2d::Costmap2D *costmap_;
+        std::vector<Node> nodes_;
+        rrt_params params_;
+        CollisionDetector collision_dect_;
+        RandomDoubleGenerator random_double_x, random_double_y;
     };
 
 };
 
 #endif // _RTT_PLANNER_RTT_PLANNER_H
-
-
--- src/rrt_planner.cpp
+++ src/rrt_planner.cpp
@@ -1,13 +1,15 @@
 
 #include <rrt_planner/rrt_planner.h>
 
-namespace rrt_planner {
+namespace rrt_planner
+{
 
-    RRTPlanner::RRTPlanner(costmap_2d::Costmap2DROS *costmap, 
-            const rrt_params& params) : params_(params), collision_dect_(costmap) {
+    RRTPlanner::RRTPlanner(costmap_2d::Costmap2DROS *costmap,
+                           const rrt_params &params) : params_(params), collision_dect_(costmap)
+    {
 
         costmap_ = costmap->getCostmap();
-        map_width_  = costmap_->getSizeInMetersX();
+        map_width_ = costmap_->getSizeInMetersX();
         map_height_ = costmap_->getSizeInMetersY();
 
         random_double_x.setRange(-map_width_, map_width_);
@@ -16,7 +18,8 @@ namespace rrt_planner {
         nodes_.reserve(params_.max_num_nodes);
     }
 
-    bool RRTPlanner::planPath() {
+    bool RRTPlanner::planPath()
+    {
 
         // clear everything before planning
         nodes_.clear();
@@ -27,22 +30,27 @@ namespace rrt_planner {
         double *p_rand, *p_new;
         Node nearest_node;
 
-        for (unsigned int k = 1; k <= params_.max_num_nodes; k++) {
+        for (unsigned int k = 1; k <= params_.max_num_nodes; k++)
+        {
 
             p_rand = sampleRandomPoint();
             nearest_node = nodes_[getNearestNodeId(p_rand)];
             p_new = extendTree(nearest_node.pos, p_rand); // new point and node candidate
 
-            if (!collision_dect_.obstacleBetween(nearest_node.pos, p_new)) {
+            if (!collision_dect_.obstacleBetween(nearest_node.pos, p_new))
+            {
                 createNewNode(p_new, nearest_node.node_id);
-
-            } else {
+            }
+            else
+            {
                 continue;
             }
 
-            if(k > params_.min_num_nodes) {
-                
-                if(computeDistance(p_new, goal_) <= params_.goal_tolerance){
+            if (k > params_.min_num_nodes)
+            {
+
+                if (computeDistance(p_new, goal_) <= params_.goal_tolerance)
+                {
                     return true;
                 }
             }
@@ -51,62 +59,95 @@ namespace rrt_planner {
         return false;
     }
 
-    int RRTPlanner::getNearestNodeId(const double *point) {
+    int RRTPlanner::getNearestNodeId(const double *point)
+    {
+        int nearest_id = 0;
+        double min_distance = std::numeric_limits<double>::max();
 
-        /**************************
-         * Implement your code here
-         **************************/
+        for (size_t i = 0; i < nodes_.size(); ++i)
+        {
+            double distance = computeDistance(nodes_[i].pos, point);
+            if (distance < min_distance)
+            {
+                min_distance = distance;
+                nearest_id = i;
+            }
+        }
 
+        return nearest_id;
     }
 
-    void RRTPlanner::createNewNode(const double* pos, int parent_node_id) {
-
+    void RRTPlanner::createNewNode(const double *pos, int parent_node_id)
+    {
         Node new_node;
-
-        /**************************
-         * Implement your code here
-         **************************/
-
+        new_node.pos[0] = pos[0];
+        new_node.pos[1] = pos[1];
+        new_node.node_id = nodes_.size();
+        new_node.parent_id = parent_node_id;
+        new_node.cost_to_go = nodes_[parent_node_id].cost_to_go + computeDistance(nodes_[parent_node_id].pos, pos);
         nodes_.emplace_back(new_node);
-        
     }
 
-    double* RRTPlanner::sampleRandomPoint() {
+    double *RRTPlanner::sampleRandomPoint()
+    {
+        // Generate a random number between 0 and 1
+        double random_value = static_cast<double>(rand()) / RAND_MAX;
 
-        /**************************
-         * Implement your code here
-         **************************/
-
-        rand_point_[0] = // ... ;
-        rand_point_[1] = // ... ;
+        // With a probability of `goal_bias`, sample the goal directly
+        if (random_value < 0.1)
+        {
+            // Return the goal as the random point
+            rand_point_[0] = goal_[0];
+            rand_point_[1] = goal_[1];
+        }
+        else
+        {
+            // Otherwise, sample a random point in the space
+            rand_point_[0] = random_double_x.generate();
+            rand_point_[1] = random_double_y.generate();
+        }
 
         return rand_point_;
     }
 
-    double* RRTPlanner::extendTree(const double* point_nearest, const double* point_rand) {
+    double *RRTPlanner::extendTree(const double *point_nearest, const double *point_rand)
+    {
+        double dx = point_rand[0] - point_nearest[0];
+        double dy = point_rand[1] - point_nearest[1];
+        double distance = sqrt(dx * dx + dy * dy);
 
-        /**************************
-         * Implement your code here
-         **************************/
+        const double fixed_extension_length = 0.25;
 
-        candidate_point_[0] = // ... ;
-        candidate_point_[1] = // ... ;
+        if (distance <= fixed_extension_length)
+        {
+            candidate_point_[0] = point_rand[0];
+            candidate_point_[1] = point_rand[1];
+        }
+        else
+        {
+            double ratio = fixed_extension_length / distance;
+            candidate_point_[0] = point_nearest[0] + dx * ratio;
+            candidate_point_[1] = point_nearest[1] + dy * ratio;
+        }
 
         return candidate_point_;
     }
 
-    const std::vector<Node>& RRTPlanner::getTree() {
+    const std::vector<Node> &RRTPlanner::getTree()
+    {
 
         return nodes_;
     }
 
-    void RRTPlanner::setStart(double *start) {
+    void RRTPlanner::setStart(double *start)
+    {
 
         start_[0] = start[0];
         start_[1] = start[1];
     }
 
-    void RRTPlanner::setGoal(double *goal) {
+    void RRTPlanner::setGoal(double *goal)
+    {
 
         goal_[0] = goal[0];
         goal_[1] = goal[1];
